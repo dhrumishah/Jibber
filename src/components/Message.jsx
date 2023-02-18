422c3704ef425aba5e4335d8f8047daaf63b91c8
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-02-18 19:42:42 +0530

    completed design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Profile from "../img/profile.png";
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src={Profile} alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        {/* <img src={Profile} alt="" /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
